cfa80720a57ea59655b6653a93e9b1efee6999e9
Ajay-javaarena <ajay16.adn@gmail.com>
2024-04-26 18:58:08 +0530

    initial commit

---
+++ src/main/java/com/nit/service/App.java
@@ -0,0 +1,14 @@
+package com.nit.service;
+
+import java.util.StringTokenizer;
+
+public class App 
+{
+	
+    public static void main( String[] args ) throws StringIndexOutOfBoundsException 
+    {
+    	StringTokenizer st = new StringTokenizer("Hello Everyone Have a nice day"," ");
+    	
+    	System.out.println("Total number of Tokens: "+st.countTokens());
+    }
+}
